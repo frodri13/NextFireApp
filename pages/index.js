087764f3b1030081078d170cf523293791a51bc4
Fabio Rodriguez <alessf13@gmail.com>
2022-05-20 19:06:58 +0100

    Loader component created

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Loader from '../components/loader'
 
 export default function Home() {
   return (
     <div>
-      <Link prefetch={false} href={{
-        pathname: "/[username]",
-        query: { username: 'fabio'},
-      }}>
-        <a>Fabio's profile</a>
-      </Link>
+      <Loader show />
     </div>
   )
 }
