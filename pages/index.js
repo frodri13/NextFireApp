7f0de00f770a46f83f71da934429c9ab51dc1665
Fabio Rodriguez <alessf13@gmail.com>
2022-05-20 19:17:17 +0100

    toast component added

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,15 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Loader from '../components/loader'
+import toast from 'react-hot-toast'
 
 export default function Home() {
   return (
     <div>
       <Loader show />
+      <button onClick={() => toast.success('Hello Toast!')}>
+        Toast Me!
+      </button>
     </div>
   )
 }
