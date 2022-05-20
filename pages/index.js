e609df13f9d5c007cbd82f662b525ba3c03ee09d
Fabio Rodriguez <alessf13@gmail.com>
2022-05-20 19:02:25 +0100

    additional pages created

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <Link prefetch={false} href={{
+        pathname: "/[username]",
+        query: { username: 'fabio'},
+      }}>
+        <a>Fabio's profile</a>
+      </Link>
+    </div>
   )
 }
