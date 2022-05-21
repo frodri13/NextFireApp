e7d81a46c54427bae4e52f4278bd9eabe77425b5
Fabio Rodriguez <alessf13@gmail.com>
2022-05-21 10:36:25 +0100

    signin and singOut created

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,45 @@
-export default function EnterPage({}) {
+import {auth, googleAuthProvider, firestore} from "../lib/firebase"
+import { doc, writeBatch, getDoc, getFirestore } from 'firebase/firestore';
+import { signInWithPopup, signInAnonymously, signOut } from 'firebase/auth';
+
+export default function EnterPage(props) {
+    const user = null;
+    const username = null;
+
+    //  1. user signed out <SignInButton />
+
+    // User signed in, but missing username <UsernameForm />
+
+    // User signed in, has username <SignOutButton />
+
     return(
         <main>
-            <h1>Sign Up!</h1>
+          {user?
+            !username ? <UsernameForm /> : <SignOutButton />
+          :
+          <SignInButton />
+          }
         </main>
     )
+}
+
+function SignInButton(){
+    const signInWithGoogle = async () => {
+        await signInWithPopup(auth, googleAuthProvider);
+    }
+
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            <img src={'/google.png'} width="30px"/>Sign in with Google
+        </button>
+    )
+}
+
+function SignOutButton(){
+    return <button onClick={() => signOut(auth)}>Sign Out</button>;
+
+}
+
+function UsernameForm() {
+
 }
