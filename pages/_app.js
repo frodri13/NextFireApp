d6b43593f45c73826abcdb068318db8ca3227759
Fabio Rodriguez <alessf13@gmail.com>
2022-05-21 10:53:30 +0100

    hook created for useUserData

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import '../styles/globals.css'
-import Navbar from '../components/navbar'
-import { Toaster } from 'react-hot-toast'
-import { UserContext } from "../lib/context"
+import '../styles/globals.css';
+import Navbar from '../components/navbar';
+import { Toaster } from 'react-hot-toast';
+import { UserContext } from "../lib/context";
+import { useUserData } from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
   return (
     <>
-      <UserContext.Provider value={{user: {}, username: 'Fabio'}}>
+      <UserContext.Provider value={userData}>
         <Navbar />
         <Component {...pageProps} />
         <Toaster />
