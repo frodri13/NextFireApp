67996716960dd09997e978762cd7d686fb8d5789
Fabio Rodriguez <alessf13@gmail.com>
2022-06-10 21:25:48 +0100

    postFeed and UserProfile components created

---
+++ components/UserProfile.js
@@ -0,0 +1,8 @@
+// UI component for user profile
+export default function UserProfile({user}) {
+    return (
+        <div className="box-center">
+
+        </div>
+    )
+}
--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,6 +1,11 @@
-export default function UserPage({}) {
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed";
+
+export default function UserProfilePage({user, posts}) {
     return(
         <main>
+            <UserProfile user={user} />
+            <PostFeed post={posts} />
         </main>
     )
 }
