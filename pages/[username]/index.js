3c75b35325be339a722ed1b666d1dfc3d027cec4
Fabio Rodriguez <alessf13@gmail.com>
2022-07-08 17:28:22 +0100

    no user error page

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,20 +1,49 @@
-import UserProfile from "../../components/UserProfile";
-import PostFeed from "../../components/PostFeed";
+import { getUserWithUsername, postToJSON, firestore } from '../../lib/firebase';
+import { query, collection, where, getDocs, limit, orderBy, getFirestore } from 'firebase/firestore';
+import UserProfile from '../../components/UserProfile';
+import Metatags from '../../components/Metatags';
+import PostFeed from '../../components/PostFeed';
 
-export async function getServerSideProps({query}) {
-    const {username} = query;
 
-    const userDoc = await getUserWithUsername(username);
+export async function getServerSideProps({ query: urlQuery }) {
+  const { username } = urlQuery;
+
+  const userDoc = await getUserWithUsername(username);
+
+  // If no user, short circuit to 404 page
+  if (!userDoc) {
     return {
-        props: {user, posts},
-    }
+      notFound: true,
+    };
+  }
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+
+    const postsQuery = query(
+      collection(getFirestore(), userDoc.ref.path, 'posts'),
+      where('published', '==', true),
+      orderBy('createdAt', 'desc'),
+      limit(5)
+    );
+    posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
 }
 
-export default function UserProfilePage({user, posts}) {
-    return(
-        <main>
-            <UserProfile user={user} />
-            <PostFeed post={posts} />
-        </main>
-    )
+export default function UserProfilePage({ user, posts }) {
+  return (
+    <main>
+      <Metatags title={user.username} description={`${user.username}'s public profile`} />
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+    </main>
+  );
 }
